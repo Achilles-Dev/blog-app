e134ab3930eee616a2b538be508b603ecc825cf6
Achilles-Dev <solohag92@gmail.com>
2022-07-06 08:38:42 +0000

    Add index | foreign_keys to likes table

--- db/migrate/20220705150517_create_likes.rb
+++ db/migrate/20220705150517_create_likes.rb
@@ -6,5 +6,9 @@ class CreateLikes < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
+    add_index :likes, :author_id
+    add_index :likes, :post_id
+    add_foreign_key :likes, :users
+    add_foreign_key :likes, :posts
   end
 end
