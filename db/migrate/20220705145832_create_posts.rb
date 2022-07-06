e58117fd6028cb896ea9636e5ef723213ccdc98a
Achilles-Dev <solohag92@gmail.com>
2022-07-06 10:27:48 +0000

    Add schema.rb file

--- db/migrate/20220705145832_create_posts.rb
+++ db/migrate/20220705145832_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.integer :author_id
+      t.integer :user_id
       t.string :title
       t.text :text
       t.integer :comments_counter
@@ -9,7 +9,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
 
       t.timestamps
     end
-    add_index :posts, :author_id
+    add_index :posts, :user_id
     add_foreign_key :posts, :users
   end
 end
--- db/migrate/20220705150212_create_comments.rb
+++ db/migrate/20220705150212_create_comments.rb
@@ -2,12 +2,12 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.integer :author_id
+      t.integer :user_id
       t.integer :post_id
 
       t.timestamps
     end
-    add_index :comments, :author_id
+    add_index :comments, :user_id
     add_index :comments, :post_id
     add_foreign_key :comments, :users
     add_foreign_key :comments, :posts
