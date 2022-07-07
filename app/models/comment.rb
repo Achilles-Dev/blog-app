e777fa9ede720c610d545c83e51c14791e02613c
Achilles-Dev <solohag92@gmail.com>
2022-07-07 10:27:50 +0000

    Add methods to Comment | Like models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def post_comements_counter_updater
+    post.comments_counter = post.comments.count
+  end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  def post_likes_counter
+    post.likes_counter = post.likes.count
+  end
 end
