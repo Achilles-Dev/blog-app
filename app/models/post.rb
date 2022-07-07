3c8330f00b293d48afb6bb3ac49720292f3668fe
Achilles-Dev <solohag92@gmail.com>
2022-07-07 16:20:25 +0000

    Add validation to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,12 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presense: true, length: { in: 1..250 }
+  validates :comments_counter, presence: true,
+                               numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true,
+                            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   def counter_updater
     user.post_counter = user.posts.count
     User.find(user.id).update(post_counter: user.posts.count)
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   has_many :likes
 
   validates :name, presence: true
-  validates :post_counter, presence: true,
-                           numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :post_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def last_three_posts
     posts.last(3)
