45fec99079c98a4068bb75db30e3a914a95a5ce9
Achilles-Dev <solohag92@gmail.com>
2022-07-07 10:22:14 +0000

    Add two methods to Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,12 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   has_many :likes
+
+  def user_posts_counter
+    user.post_counter = user.posts.count
+  end
+
+  def last_five_comments
+    comments.last(5)
+  end
 end
