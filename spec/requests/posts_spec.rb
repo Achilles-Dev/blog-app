36edfd918024acc66d212ae90cf1064187933565
Achilles-Dev <solohag92@gmail.com>
2022-07-11 23:34:59 +0000

    Add tests for post controller method #show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Here is a list of posts for a given user')
     end
   end
+
+  describe 'GET #show' do
+    before(:example) { get '/users/1/posts/1' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'returns text in response body' do
+      expect(response.body).to include("Here are the details of a given user's post")
+    end
+  end
 end
