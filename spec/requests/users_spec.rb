38f410cd31184cbf90f02596cce98af248cd4f3a
Achilles-Dev <solohag92@gmail.com>
2022-07-11 23:31:19 +0000

    Add tests for user controller method #show

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -16,4 +16,20 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('Here is a list all users')
     end
   end
+
+  describe 'GET #show' do
+    before(:example) { get '/users/1' }
+
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders 'show' template" do
+      expect(response).to render_template(:show)
+    end
+
+    it 'returns text in response body' do
+      expect(response.body).to include('Here are the details of the selected user')
+    end
+  end
 end
