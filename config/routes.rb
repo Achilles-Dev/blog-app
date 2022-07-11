65826c4380d305fadca6e05cff64628f098c2699
Achilles-Dev <solohag92@gmail.com>
2022-07-11 10:33:41 +0000

    Add routes for users and user_posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get 'users/:user_id/posts', to: 'posts#index'
+
+  get 'users/:user_id/posts/:id', to: 'posts#show'
+
+  get 'users', to: 'users#index'
+
+  get 'users/:id', to: 'users#show'
 end
