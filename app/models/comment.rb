d3d9a00af44bbd25c0fc33b96d28c610dd873248
Achilles-Dev <solohag92@gmail.com>
2022-07-13 08:41:02 +0000

    Refactor to follow best-practices

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,6 +3,6 @@ class Comment < ApplicationRecord
   belongs_to :post
 
   def counter_updater
-    post.comments_counter = post.comments.length
+    post.update(comments_counter: post.comments.length)
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :post
 
   def counter_updater
-    post.likes_counter = post.likes.length
+    post.update(likes_counter: post.likes.length)
   end
 end
