7af21282b95db4481db5882c757e43c3a0ca0407
Achilles-Dev <solohag92@gmail.com>
2022-07-13 16:06:11 +0000

    Add method to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,13 @@ class PostsController < ApplicationController
     @user_posts = @user.posts
   end
 
+  def new
+    post = Post.new
+    respond_to do |format|
+      format.html { render :new, locals: { post: } }
+    end
+  end
+
   def show
     @user_post = Post.find_by(user_id: params[:user_id], id: params[:id])
   end
