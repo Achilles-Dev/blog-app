dc7bba483da7ac456f83653740d7321d601ccdfa
Achilles-Dev <solohag92@gmail.com>
2022-07-14 01:04:12 +0000

    Add method to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,28 @@ class PostsController < ApplicationController
   end
 
   def new
+    @id = current_user.id
     post = Post.new
     respond_to do |format|
       format.html { render :new, locals: { post: } }
+    end
+  end
+
+  def create
+    @id = current_user.id
+    post = Post.new(title: params[:title], text: params[:text], user: current_user, comments_counter: 0,
+                    likes_counter: 0)
+    respond_to do |format|
+      format.html do
+        if post.save
+          post.counter_updater
+          flash[:success] = 'Post saved successfully'
+          redirect_to "/users/#{@id}/posts"
+        else
+          flash.now[:error] = 'Error: Post could not be saved'
+          render :new, locals: { post: }
+        end
+      end
     end
   end
 
