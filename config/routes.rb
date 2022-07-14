322f54ac91b05e46b7d0b15c1ca3db49e134c5de
Achilles-Dev <solohag92@gmail.com>
2022-07-14 14:11:47 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :create, :new, :show] do
-      resources :comments, only: [:create]
-      resources :likes, only: [:create]
+      resources :comments, only: [:create, :new]
+      resources :likes, only: [:create, :new]
     end
   end
 end
