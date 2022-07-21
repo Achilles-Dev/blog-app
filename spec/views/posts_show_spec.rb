3807eb4a6c138bad4536224b8fcb897ee1305d04
Achilles-Dev <solohag92@gmail.com>
2022-07-21 10:33:44 +0000

    Update posts_show_spec.rb file

--- spec/views/posts_show_spec.rb
+++ spec/views/posts_show_spec.rb
@@ -6,24 +6,28 @@ RSpec.describe 'PostsShowPages', type: :system do
   end
 
   before(:each) do
-    @user = create(:user, name: 'James')
-    login_as @user
-    @other_user = create(:user, name: 'Jane')
+    @user = User.create!(name: 'Sam', bio: 'Full stack developer', email: 'sam@gmail.com',
+                         password: 'sam22021', post_counter: 0, confirmed_at: Time.now)
+
+    # login_as @user
+    @user2 = User.create!(name: 'Solomon', bio: 'Full stack developer', post_counter: 0,
+                          email: 'user2@example.com', password: 'password2', confirmed_at: Time.now)
     (1..6).each do |n|
-      post = create(:post, user_id: @other_user.id, title: "Post #{n}")
-      post.counter_updater(@other_user)
+      post = Post.create(user_id: @user2.id, title: "Post #{n}", text: "Post content #{n}",
+                         comments_counter: 0, likes_counter: 0)
+      post.counter_updater
     end
-    @other_user.posts.each do |post|
-      comment = create(:comment, post_id: post.id, user_id: @user.id)
-      comment.counter_updater(post)
+    @user2.posts.each do |post|
+      comment = Comment.create(post:, user: @user, text: 'Hi Tom!')
+      comment.counter_updater
     end
-    @post = @other_user.posts.first
-    visit "/users/#{@other_user.id}/posts/#{@post.id}"
+    @post = @user2.posts.first
+    visit "/users/#{@user2.id}/posts/#{@post.id}"
   end
 
   describe 'Page content' do
     it 'should have name of who wrote the post' do
-      expect(page).to have_content(@other_user.name)
+      expect(page).to have_content(@user2.name)
     end
     it "should display a posts's title" do
       expect(page).to have_content(@post.title)
