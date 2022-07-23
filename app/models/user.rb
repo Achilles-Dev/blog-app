0d2a7f21ed9e4da0d7539651a241ef570207b251
Achilles-Dev <solohag92@gmail.com>
2022-07-23 02:56:01 +0000

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,4 +22,8 @@ class User < ApplicationRecord
   def admin?
     role == 'admin'
   end
+
+  def generate_jwt
+    JWT.encode({ id:, exp: 60.days.from_now.to_i }, Rails.application.secrets.secret_key_base)
+  end
 end
